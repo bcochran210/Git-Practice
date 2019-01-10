529be9a1d364a52cd48d44658b2c7da5d2742caf
Bailey Cochran <bcochran@stu.jsu.edu>
2019-01-10 15:07:12 -0600

    Initial Commit

---
+++ Class1.java
@@ -0,0 +1,9 @@
+public class Class1 {
+
+	private void f1() {
+
+		//placeholder method (v1.0)
+	
+	}
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public cass HelloWorld {
+
+	public static void main(String[] args){
+ 
+        class c1 = new Class1();
+        class c2 = new Class2();
+        system.out.println("Hello World");
+ 
+    }
+
+}
